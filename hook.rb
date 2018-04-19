85a6477bf7969bc1189fa5204c6354b93667c80d
jkaplon <jody@kaplon.us>
2018-04-19 16:00:07 -0400

    Remove old/unused routes for IoT or blog side-projects (ended up using node.js instead); closes #6.

--- hook.rb
+++ hook.rb
@@ -8,20 +8,8 @@ output = ""
 
 puts "Process ID: #{Process.pid}"
 
-get '/particle062015' do
-    'OK'
-end
-
-post '/particle062015' do
-    payload = JSON.parse(params[:payload])
-    post_event = payload["postEvent"]
-    source = payload["source"]
-    output += "\nReceived from Sparticle, postEvent = #{post_event}, source = #{source}"
-    puts output
-end
-
 get '/8c18e7e321e683854951a696318a55a4' do
-'OK'
+    'OK'
 end
 
 post '/8c18e7e321e683854951a696318a55a4/alytfeed' do
@@ -41,31 +29,4 @@ post '/8c18e7e321e683854951a696318a55a4/alytfeed' do
     puts output
     output
     'OK'
-end
-
-post '/8c18e7e321e683854951a696318a55a4/jodyblog' do
-    payload = JSON.parse(params[:payload])
-    site_name = "blog.kaplon.us"
-
-    # Payload["name"] is name given to doc at time of publication from draftin.com.
-    Dir.chdir("#{tmp_dir}#{site_name}/_posts/")
-    file_name = payload["name"]
-    file_content_front_matter_plus_md = "---\ntitle: " + payload["name"] + "\ncreated_at: " + payload["created_at"] + "\nupdated_at: " + payload["updated_at"] + "\n---\n" + payload["content"]
-    File.write("#{file_name}.md", file_content_front_matter_plus_md) 
-
-    # Need to get rest of jekyll dir's setup...not sure if better to do under /tmp/webhooks/blog.kaplon.us/
-    # or maybe cp newly published posts from /tmp/webhooks/blog.kaplon.us/_posts/ to more permaent jekyll location.
-
-    if File.exists?("#{tmp_dir}#{site_name}/Gemfile")
-        output += "\n\nBundling Gems\n"
-        output += `pushd #{tmp_dir}#{site_name}; bundle; popd;`
-    end
-
-    build_cmd = "jekyll build --source #{tmp_dir}#{site_name} --destination #{web_dir}#{site_name}/public_html"
-
-    output += "\n\nBuilding from source using Jekyll\n\n"
-    output += `#{build_cmd}`
-
-    puts output
-    output
 end
