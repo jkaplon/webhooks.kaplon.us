728dd9cb98d01238ad23d65e03aea53640adccb2
jkaplon <jody@kaplon.us>
2018-04-25 17:03:19 -0400

    Basic node/express server working; index page loading in browser; GitHub webhook receives 200/OK response.

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.text());   // Use defaults for now, size limit is 100kb.
 app.use(bodyParser.urlencoded({ extended: true }));   // Also need url encoding to handle login form.
 var winston = require('winston');
-winston.add(winston.transports.File, { filename: './logs/notes.kaplon.us.log', maxsize: 5000000 });  // 5MB
+winston.add(winston.transports.File, { filename: './logs/webhooks.kaplon.us.log', maxsize: 5000000 });  // 5MB
 var fileSystem = require('fs');
 
 app.get('/', function(req, res){
     winston.info("GET /");
     // Respond with static file, contents will be loaded via websocket.
-    res.sendFile('index.html');
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/alytfeed', function(req, res){
+    winston.info("GET /");
+    // Respond with static file, contents will be loaded via websocket.
+    res.sendFile('index.html', { root: __dirname });
 });
 
 app.post('/alytfeed', function(req, res) {
     winston.info('Potential post from GitHub webhook.');
-    res.send(200);  // TODO: look up this syntax
+    res.status(200).send('OK');
 
     // TODO: verify GitHup HMAC header (figure out how to store secret word in env-var or config file).
     // then `rm -rf` the /tmp dir (may need to edit Dockerfile to add dir
@@ -27,6 +33,6 @@ app.post('/alytfeed', function(req, res) {
     // ?where would i rather look 1st for an error?...logs from this app, or GitHub-webhook-dashboard?
 });
 
-http.listen(4567, function() {
+app.listen(4567, function() {
     winston.info("Started on PORT 4567");
 });
